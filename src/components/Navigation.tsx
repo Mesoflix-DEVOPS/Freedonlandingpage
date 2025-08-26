130924ce12d573aaf2e47ce4d772b50a653e7f43
Ceofreddy <fkmukuha3@gmail.com>
2025-08-26 13:22:32 +0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -74,7 +74,7 @@ const Navigation = () => {
               asChild
             >
               <a 
-                href="https://deriv.com" 
+                href="https://track.deriv.com/_TH5LC8pLrKEKqFKZ7JdnQ2Nd7ZgqdRLk/1/" 
                 target="_blank" 
                 rel="noopener noreferrer"
               >
